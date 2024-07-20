bc6e41e3abde3884bb3c69631f95abb19a54ceb2
Tanguy <tngblondin.prog@gmail.com>
2024-07-20 19:14:28 +0200

    Implementing basic calculation

--- calculatrice/src/main.rs
+++ calculatrice/src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
     println!("Hello, world!");
+
+    println!("10 + 3 = {}", calculate(10, 3, '+'));
+    println!("10 - 3 = {}", calculate(10, 3, '-'));
+    println!("10 * 3 = {}", calculate(10, 3, '*'));
+    println!("10 / 3 = {}", calculate(10, 3, '/'));
+}
+
+fn calculate(left_member: i32, right_member: i32, operator: char) -> i32{
+    let mut result = 0;
+
+    if operator == '+' {
+        result = left_member + right_member;
+    }
+
+    if operator == '-' {
+        result = left_member - right_member;
+    }
+
+    if operator == '*' {
+        result = left_member * right_member;
+    }
+
+    if operator == '/' {
+        result = left_member / right_member;
+    }
+
+    result
 }
