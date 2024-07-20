471431332345fe55d999f1e01633f4ba16662e76
Tanguy <tngblondin.prog@gmail.com>
2024-07-20 19:35:10 +0200

    Implementing read user input

--- calculatrice/src/main.rs
+++ calculatrice/src/main.rs
@@ -1,5 +1,19 @@
+use std::io::{self, Write};
+
 fn main() {
     println!("Hello, world!");
+
+    let mut input = String::new();
+
+    print!("Enter your operation : ");
+    let _ = io::stdout().flush();
+    
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    println!("the user input is : {}", input);
+
 
     println!("10 + 3 = {}", calculate(10, 3, '+'));
     println!("10 - 3 = {}", calculate(10, 3, '-'));
