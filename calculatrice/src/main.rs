1c2abee4f35ece8897affc02ca4ac162e0a68100
Tanguy <tngblondin.prog@gmail.com>
2024-07-21 10:17:16 +0200

    Spliting the operation

--- calculatrice/src/main.rs
+++ calculatrice/src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{self, Write};
+use std::{io::{self, Write}, vec};
 
 fn main() {
     println!("Hello, world!");
@@ -18,14 +18,42 @@ fn main() {
 
     println!("input after cleaning : {}", operation);
 
+    let split_operation = split_operation(&operation);
+    println!("The operation has been split :");
+    for i in 0..split_operation.len() {
+        println!("{}", split_operation[i]);
+    }
+
+
     println!("10 + 3 = {}", calculate(10, 3, '+'));
     println!("10 - 3 = {}", calculate(10, 3, '-'));
     println!("10 * 3 = {}", calculate(10, 3, '*'));
     println!("10 / 3 = {}", calculate(10, 3, '/'));
+
 }
 
 fn clean_input(input: &mut String) -> String{
     input.replace(' ', "")
+}
+
+fn is_a_symbol(c: &char) -> bool {
+    ['+', '-', '*', '/'].contains(c)
+}
+
+fn split_operation(operation: &String) -> Vec<&str> {
+    let mut v: Vec<&str> = Vec::new();
+
+    let mut last_symbol = 0;
+    for (index, c) in operation.chars().enumerate() {
+        if is_a_symbol(&c) || index == operation.len() - 1 {
+            v.push(operation.get(last_symbol..index).expect("Error"));
+            last_symbol = index;
+            v.push(operation.get(last_symbol..last_symbol + 1).expect("Error"));
+            last_symbol = index + 1;
+        }
+    }
+
+    v
 }
 
 fn calculate(left_member: i32, right_member: i32, operator: char) -> i32{
